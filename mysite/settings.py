03193ab54ce2006b8d00a3843c563635e57cbac3
Jatin PANDEY <jatinpandeywork@gmail.com>
2022-09-11 13:08:01 +0530

    workibg

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,23 +84,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': 'skilldslive',
-#             'USER': 'skilldsuser',
-#             'PASSWORD': 'skilldsis1',
-#             'HOST': 'ec2-13-235-22-51.ap-south-1.compute.amazonaws.com',
-#             'PORT': '5432',
-#         },
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE':  'django.db.backends.sqlite3',
-        "NAME" :  os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'stackunfold',
+            'USER': 'stackunfold',
+            'PASSWORD': 'Destiney@123',
+            'HOST': 'stackunfold.com',
+            'PORT': '5432',
+        },
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE':  'django.db.backends.sqlite3',
+#         "NAME" :  os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 
